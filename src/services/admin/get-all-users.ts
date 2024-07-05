437ad7dea5b618d14cabab7faaf0dabb944d2c90
devedsonalves <devedsonalves@gmail.com>
2024-07-05 08:34:54 -0300

    feat: create route "get all users"

---
+++ src/services/admin/get-all-users.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { prisma } from "../../lib/prisma";
+
+async function getAllUsers(
+  request: Request, 
+  response: Response
+) {
+  const users = await prisma.user.findMany()
+    .then(users => {
+      return response.status(200).json(users)
+    })
+    .catch(() => {
+      return response.status(500).json({ error: "Internal Server Error"})
+    })
+}
+
+export default getAllUsers
