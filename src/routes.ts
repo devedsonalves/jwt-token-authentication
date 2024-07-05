36688758315189eb5f54dc74ea7628e1ecb4c80c
devedsonalves <devedsonalves@gmail.com>
2024-07-05 08:36:30 -0300

    feat: create route "get all users"

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express"
 import loginUser from "./services/auth/login-user"
+import { authMiddleware } from "./middlewares/auth"
 import registerUser from "./services/auth/register-user"
 import getUserByToken from "./services/get-user-by-token"
-import { authMiddleware } from "./middlewares/auth"
+import getAllUsers from "./services/admin/get-all-users"
 
 const routes = Router()
 
 routes.post('/register', registerUser)
 routes.post('/login', loginUser)
 
-// protected routes
 routes.get('/user', authMiddleware, getUserByToken)
+routes.get('/users', authMiddleware, getAllUsers)
 
 export default routes
